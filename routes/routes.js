8bc325844f8f51f22455b7ae41bb9c972555a5d6
HilsonF <hilson_francis@Hilsons-MacBook-Pro.local>
2016-09-23 15:49:53 -0400

    accidently removed routes folder on server

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-
+//
 module.exports = function(app, passport, multer, multerResizer) {
 
 var manager = require('../models/manager.js');
@@ -120,7 +120,6 @@ app.post('/uploadImages', multipleupload, function(req, res, next) {
   if (files) {
   files.forEach(function(file){
     var category = req.body.category;
-    //var fileUrl = file.path;
     var str = file.path; 
     var res = str.slice(0,str.indexOf('.'));
     var fileUrl = res + "_optimized.jpg";
