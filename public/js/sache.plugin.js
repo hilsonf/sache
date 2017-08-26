19904e2d4dd732400b45474d71a452ed0769cba5
HilsonF <hfrancis103@gmail.com>
2017-08-26 04:39:59 -0400

    fixed plugin issue

--- public/js/sache.plugin.js
+++ public/js/sache.plugin.js
@@ -1,6 +1,6 @@
 daysmart_acc = "e69b49d3-fd2b-46a3-98a7-d51bdde5de74"; 
 daysmart_iframe_width =1416; 
-daysmart_iframe_height = 0;
+daysmart_iframe_height =600;
 
  (function () {
     'use strict';
